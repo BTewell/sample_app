dd14350ec2958de46203b5ab876bbf477ca5cd2a
“Brett <bretttewell@gmail.com>
2019-02-28 10:09:37 -0600

    add arlo and town url to app

--- app/controllers/api/sample_pages_controller.rb
+++ app/controllers/api/sample_pages_controller.rb
@@ -3,4 +3,10 @@ class Api::SamplePagesController < ApplicationController
   def hello_method
     render json: {message: "Hello!"}
   end
+  def movement_method
+    render json: {message: "You can get anything you want at Alice's Restaurant."}
+  end
+  def city_method
+    render html: "Stockbridge, Massachusetts"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get '/hello_url' => 'sample_pages#hello_method'
+    get 'arlo_url' => 'sample_pages#movement_method'
+    get '/town_url' => 'sample_pages#city_method'
   end
 end
